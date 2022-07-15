ad5c3aa976ea0e0b08ed29323503e4309cf12231
Adèle <aguessya@yahoo.fr>
2022-07-15 15:15:26 +0200

    clean files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { CardComponent } from './card/card.component';
-import { CoursesListComponent } from './courses-list/courses-list.component';
-import { InputParentComponent } from './input/input-parent/input-parent.component';
-import { InputChildComponent } from './input/input-child/input-child.component';
-import { MenuComponent } from './menu/menu.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { ROUTES } from './app.routes';
 import { InscriptionFormComponent } from './inscription-form/inscription-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent,
-    CardComponent,
-    CoursesListComponent,
-    InputParentComponent,
-    InputChildComponent,
-    MenuComponent,
-    SignUpComponent,
+
     InscriptionFormComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     RouterModule.forRoot(ROUTES),
     ReactiveFormsModule
   ],
